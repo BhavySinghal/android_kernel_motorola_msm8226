99c77879041e9fdae90d67a5bf7133d7f516dce0
Gustavo <gustavo.santos0106@gmail.com>
2015-02-22 00:33:35 -0300

    logger: Add sys kernel interface to configure android logcat
    Signed-off-by: Gustavo <gustavo.santos0106@gmail.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -46,6 +46,9 @@ static DEFINE_SPINLOCK(log_lock);
 static struct work_struct write_console_wq;
 static struct tasklet_struct schedule_work_tasklet;
 
+static unsigned int log_enabled = 1;
+module_param(log_enabled, uint, S_IWUSR | S_IRUGO);
+
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  *
@@ -491,7 +494,7 @@ ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct timespec now;
 	ssize_t ret = 0;
 
-	if (!log_enabled && !log_always_on)
+	if (!log_enabled)
 		return 0;
 
 	getnstimeofday(&now);
